e7119ed41b4f36422e4eab470a0a68e2bb166864
gothwalritu <gothwalritu@gmail.com>
2022-09-08 11:38:14 -0700

    13.5.5 Module

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,6 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
+<<<<<<< HEAD
 let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 let line = [
@@ -20,10 +21,33 @@ L.polyline(line, {
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+=======
+let map = L.map('mapid').setView([30, 30], 2);
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+>>>>>>> 3ae93f675b36bd05943850f444700e14bccca5bc
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
 // Then we add our 'graymap' tile layer to the map.
+<<<<<<< HEAD
 streets.addTo(map);
+=======
+streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/gothwalritu/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
+
+
+>>>>>>> 3ae93f675b36bd05943850f444700e14bccca5bc
